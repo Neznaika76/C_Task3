cd017c6f6185f83c1b9cf56aa24d553d18460c44
Neznaika76 <n9032692530@gmail.com>
2023-12-03 15:57:26 +0300

    Задача 4

--- Task3/main.c
+++ Task3/main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 // Задача 1
-/*
+
 int main(int argc, const char * argv[]) {
     int a,b,c,d,e;
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
     max1 = max1 > max2 ? max1 : max2;
     printf("%d\n",max1);
     return 0;
-}*/
+}
 
 // Задача 2
 /*
@@ -32,4 +32,31 @@ int main(int argc, const char * argv[]) {
     min1 = min1 < min2 ? min1 : min2;
     printf("%d\n",min1);
     return 0;
+} */
+
+// Задача 3
+/*
+int main(int argc, char **argv)
+{
+int a, b, c;
+scanf("%d%d%d", &a, &b, &c);
+printf("%s\n", a<b && b<c ? "YES" : "NO");
+return 0;
+} */
+
+// Задача 4
+/*
+int main(int argc, char **argv)
+{
+int a;
+scanf("%d", &a);
+switch(a)
+    {
+        case 1:case 2:case 12: printf("Зима\n"); break;
+        case 3:case 4:case 5: printf("Весна\n"); break;
+        case 6:case 7:case 8: printf("Лето\n"); break;
+        case 9:case 10:case 11: printf("Осеньn\n"); break;
+        default: printf("Неверное число\n");
+    }
+return 0;
 } */
