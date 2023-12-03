c438c8a0a4eed239914b62b763e9dfd2cb736ec9
Neznaika76 <n9032692530@gmail.com>
2023-12-03 15:25:28 +0300

    Initial Commit

---
+++ Task3/main.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  Task3
+//
+//  Created by Pavel iPro on 03.12.2023.
+//
+
+#include <stdio.h>
+
+// Задача 1
+/*
+int main(int argc, const char * argv[]) {
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    int max1 = a>b ? a : b;
+    int max2 = c>d ? c : d;
+    max1 = max1>e ? max1 : e;
+    max1 = max1 > max2 ? max1 : max2;
+    printf("%d\n",max1);
+    return 0;
+}*/
+
+// Задача 2
+/*
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    int min1 = a < b ? a : b;
+    int min2 = c < d ? c : d;
+    min1 = min1 < e ? min1 : e;
+    min1 = min1 < min2 ? min1 : min2;
+    printf("%d\n",min1);
+    return 0;
+} */
